d048aad9566dbcb73afe8106467c42f25272bbf8
keisukeponpoko <k.suke.jp1990@gmail.com>
2017-03-20 23:53:38 +0900

    commit

--- seniorEat/settings.py
+++ seniorEat/settings.py
@@ -84,11 +84,23 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'senior_eat',
         'USER': 'root',
-        'PASSWORD': 'keisuke0415',
+        'PASSWORD': '',
         'HOST': '',
         'PORT': '',
     }
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# update db settings
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=400)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
